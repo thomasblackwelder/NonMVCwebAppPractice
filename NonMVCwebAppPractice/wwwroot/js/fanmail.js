551647a62cee143eb11ef54d830673bef6728046
thomasblackwelder <83183821+thomasblackwelder@users.noreply.github.com>
2022-01-10 11:52:48 -0700

    Add project files.

---
+++ NonMVCwebAppPractice/wwwroot/js/fanmail.js
@@ -0,0 +1,19 @@
+﻿
+
+//document.getElementById("btnSend").addEventListener("click", function () {
+//    alert("This Worked!" + "\n" + "From: " + document.getElementById("txtFrom").value + "\n" +
+//        "Subject: " + document.getElementById("txtSub").value + "\n" + "Message: " +
+//        document.getElementById("txtMes").value
+//    );
+//})
+
+$("#btnSend").click(function () {
+    alert("From: " + $("#txtFrom").val() +
+        "\n" +
+        "Subject: " + $("#txtSub").val() +
+        "\n" +
+        "Message: " + $("#txtMes").val()
+    );
+
+    $("#pic").fadeOut("slow");
+})
